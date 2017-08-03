46594b7ad1d4928432b4072aaf7ef09e0ac3a50a
Stephan Florquin <stephan.florquin@gmail.com>
2017-08-03 14:39:21 +0200

    agent-basic-js: update for 0.1.0-rc1
    * Update versions
    * Add validation
    * Fix Tendermint data dir
    * Simplify actions

--- agent-basic-js/files/agent/lib/actions.js
+++ agent-basic-js/files/agent/lib/actions.js
@@ -10,39 +10,26 @@ module.exports = {
       return this.reject('a title is required');
     }
 
-    this.state.title = title;
-    this.state.messages = [];
-    this.state.updatedAt = Date.now();
-    this.meta.priority = 0;
+    this.state = {
+      title: title
+    };
 
     this.append();
   },
 
-  addMessage: function(message, author) {
-    if (!message) {
-      return this.reject('a message is required');
+  message: function(body, author) {
+    if (!body) {
+      return this.reject('a body is required');
     }
 
     if (!author) {
       return this.reject('an author is required');
     }
 
-    this.state.messages.push({ message: message, author: author });
-    this.state.updatedAt = Date.now();
-    this.meta.priority++;
-
-    this.append();
-  },
-
-  addTag: function(tag) {
-    if (!tag) {
-      return this.reject('a tag is required');
-    }
-
-    this.meta.tags = this.meta.tags || [];
-    this.meta.tags.push(tag);
-    this.state.updatedAt = Date.now();
-    this.meta.priority++;
+    this.state = {
+      body: body,
+      author: author
+    };
 
     this.append();
   }
