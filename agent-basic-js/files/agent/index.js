a0b185e75bbdcfb4f9f490fbf30b6080d0792888
t-bast <bastien@stratumn.com>
2017-10-18 16:53:13 +0200

    agent-basic-js: create processes from all the files in lib/action

--- agent-basic-js/files/agent/index.js
+++ agent-basic-js/files/agent/index.js
@@ -1,51 +1,70 @@
 {{- $fossilizer := (input "developmentFossilizer") -}}
 // This file creates an Express server and mounts the agent on it.
 
-var crypto = require('crypto');
-var express = require('express');
-var Agent = require('stratumn-agent');
-var plugins = Agent.plugins;
-
-// Load actions.
-// Assumes your actions are in ./lib/actions.
-{{- range $index, $proc := (input "process")}}
-var actions_{{$proc}} = require('./lib/actions-{{$proc}}');
-{{- end}}
+const fs = require('fs');
+const crypto = require('crypto');
+const express = require('express');
+const Agent = require('stratumn-agent');
+const plugins = Agent.plugins;
 
 // Create an HTTP store client to save segments.
 // Assumes an HTTP store server is available on env.STRATUMN_STORE_URL or http://store:5000.
-var storeHttpClient = Agent.storeHttpClient(process.env.STRATUMN_STORE_URL || 'http://store:5000');
+const storeHttpClient = Agent.storeHttpClient(process.env.STRATUMN_STORE_URL || 'http://store:5000');
 
 {{- if eq $fossilizer "none" }}
 // Do not use a fossilizer.
-var fossilizerHttpClient = null;
+const fossilizerHttpClient = null;
 {{- else }}
 // Create an HTTP fossilizer client to fossilize segments.
 // Assumes an HTTP fossilizer server is available on env.STRATUMN_FOSSILIZER_URL or http://fossilizer:6000.
-var fossilizerHttpClient = Agent.fossilizerHttpClient(process.env.STRATUMN_FOSSILIZER_URL || 'http://fossilizer:6000');
+const fossilizerHttpClient = Agent.fossilizerHttpClient(process.env.STRATUMN_FOSSILIZER_URL || 'http://fossilizer:6000');
 {{- end}}
 
-
 // Create an agent.
-var agentUrl = process.env.STRATUMN_AGENT_URL || 'http://localhost:3000';
-var agent = Agent.create({
+const agentUrl = process.env.STRATUMN_AGENT_URL || 'http://localhost:3000';
+const agent = Agent.create({
   agentUrl: agentUrl,
 });
 
-// Adds all processes from a name, its actions, the store client, and the fossilizer client.
-// As many processes as one needs can be added. A different storeHttpClient and fossilizerHttpClient may be used.
-{{- range $index, $proc := (input "process")}}
-agent.addProcess('{{$proc}}', actions_{{$proc}}, storeHttpClient, fossilizerHttpClient, {
+// List of plugins used for our process actions
+const processPlugins = [plugins.agentUrl(agentUrl), plugins.actionArgs, plugins.stateHash];
+
+// Load process actions.
+// Assumes your action files are in ./lib/actions and export a 'name' field.
+fs.readdir('./lib/actions', (err, processFiles) => {
+  if (err) {
+    console.error(`Cannot load process actions: ${err}`);
+    return;
+  }
+
+  processFiles.forEach(file => {
+    const actions = require(`./lib/actions/${file}`);
+    if (!actions.name) {
+      console.log(`Process ${file} doesn't export a 'name' field. Skipping...`);
+      return;
+    }
+
+    try {
+      agent.addProcess(
+        actions.name,
+        actions,
+        storeHttpClient,
+        fossilizerHttpClient,
+        {
 {{- if ne $fossilizer "none" }}
-  // the fossilizer must be able to reach the agent via this url
-  evidenceCallbackUrl: process.env.STRATUMN_EVIDENCE_CALLBACK_URL || process.env.STRATUMN_AGENT_URL || 'http://agent:3000',
-  // change to a unique salt
-  salt: process.env.STRATUMN_SALT || crypto.randomBytes(32).toString('hex'),
+          // the fossilizer must be able to reach the agent via this url
+          evidenceCallbackUrl: process.env.STRATUMN_EVIDENCE_CALLBACK_URL || process.env.STRATUMN_AGENT_URL || 'http://agent:3000',
+          // change to a unique salt
+          salt: process.env.STRATUMN_SALT || crypto.randomBytes(32).toString('hex'),
 {{- end}}
-  // plugins you want to use
-  plugins: [plugins.agentUrl(agentUrl), plugins.actionArgs, plugins.stateHash]
+          plugins: processPlugins
+        }
+      );
+    } catch (err) {
+      console.error(`Could not load process: ${actions.name}`);
+    }
+  });
 });
-{{- end}}
 
 // Creates an HTTP server for the agent with CORS enabled.
 var agentHttpServer = Agent.httpServer(agent, { cors: {} });
