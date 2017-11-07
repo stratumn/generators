69984ffa0715787745816b3b73bf25566f942793
piedup <pierreduperrin@gmail.com>
2017-11-07 11:07:00 +0100

    updated install.sh

--- agent-basic-js/files/fabric/scripts/install.sh
+++ agent-basic-js/files/fabric/scripts/install.sh
@@ -1,53 +1,52 @@
+#!/bin/sh
+
 MAX_RETRY=5
 DELAY=1
-COUNTER=1
 
 verifyResult () {
 	if [ $1 -ne 0 ] ; then
-		echo "!!!!!!!!!!!!!!! "$2" !!!!!!!!!!!!!!!!"
-    echo "========= ERROR !!! FAILED to initialize network ==========="
-		echo
+		echo "!!!!!!!!!!!!!!! "$2" !!!!!!!!!!!!!!!!" 1>&2
+    	echo "========= ERROR !!! FAILED to initialize network ===========" 1>&2
+		echo 1>&2
    		exit 1
 	fi
 }
 
 createChannelWithRetry () {
-	peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f $CONFIG_PATH/channel.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA >&log.txt
-	res=$?
-	# cat log.txt
-	if [ $res -ne 0 -a $COUNTER -lt $MAX_RETRY ]; then
-		COUNTER=` expr $COUNTER + 1`
+	counter=0
+	while [ $counter -lt $MAX_RETRY ]; do
+		peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f $CONFIG_PATH/channel.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA >&log.txt
+		res=$?
+		if [ $res == 0 ]; then
+			break
+		fi
 		echo "Failed to create channel, retrying in $DELAY seconds"
 		sleep $DELAY
-		createChannelWithRetry
-	else
-		COUNTER=1
-	fi
+		((++counter))
+	done
 	verifyResult $res "After $MAX_RETRY attempts, failed to create channel"
 }
 
 joinWithRetry () {
-	peer channel join -b mychannel.block >&log.txt
-	res=$?
-	# cat log.txt
-	if [ $res -ne 0 -a $COUNTER -lt $MAX_RETRY ]; then
-		COUNTER=` expr $COUNTER + 1`
+	counter=0
+	while [ $counter -lt $MAX_RETRY ]; do
+		peer channel join -b mychannel.block >&log.txt
+		res=$?
+		if [ $res == 0 ]; then
+			break
+		fi
 		echo "Failed to join peer to channel, retrying in $DELAY seconds"
 		sleep $DELAY
-		joinWithRetry
-	else
-		COUNTER=1
-	fi
+		((++counter))
+	done
 	verifyResult $res "After $MAX_RETRY attemps, failed to join peer to channel"
 }
 
 instantiateIfNeeded () {
 	peer chaincode query -C mychannel -n pop -c '{"Function":"GetValue","Args":["none"]}' >/dev/null
-	res=$?
-	if [ $res -ne 0 ]; then 
+	if [ $? -ne 0 ]; then 
 		peer chaincode instantiate -o orderer.example.com:7050 --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA -C $CHANNEL_NAME -n pop -v 1.0 -c '{"Args":[]}' -P "AND ('Org1MSP.member')"
-		res=$?
-		verifyResult $res "Chaincode instantiation on peer on channel '$CHANNEL_NAME' failed"
+		verifyResult $? "Chaincode instantiation on peer on channel '$CHANNEL_NAME' failed"
 		echo "===================== Chaincode Instantiation on peer on channel '$CHANNEL_NAME' is successful ===================== "
 	fi
 }
@@ -62,14 +61,12 @@ echo "===================== PEER joined on the channel \"$CHANNEL_NAME\" =======
 
 echo "Updating anchor peer for org1..."
 peer channel update -o orderer.example.com:7050 -c $CHANNEL_NAME -f $CONFIG_PATH/Org1MSPanchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
-res=$?
-verifyResult $res  "Anchor peer update failed"
+verifyResult $?  "Anchor peer update failed"
 echo "===================== Anchor peers for org \"$CORE_PEER_LOCALMSPID\" on \"$CHANNEL_NAME\" is updated successfully ===================== "
 
 echo "Installing chaincode on peer0.org1.example.com..."
 peer chaincode install -n pop -v 1.0 -p github.com/stratumn/chaincode/pop
-res=$?
-verifyResult $res "Chaincode installation on remote peer has Failed"
+verifyResult $? "Chaincode installation on remote peer has Failed"
 echo "===================== Chaincode is installed on remote peer ===================== "
 
 echo "Instantiate chaincode..."
