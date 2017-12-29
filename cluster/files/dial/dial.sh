113b1770bcb72bfaa26169947e0a2539fa510f65
t-bast <bastien@stratumn.com>
2017-12-29 16:01:27 +0100

    Add dial docker image

---
+++ cluster/files/dial/dial.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# --------------------------------------
+# - Dial script for tendermint network -
+# --------------------------------------
+
+# The script calls `/dial_seeds` on all nodes in the network.
+# In a network of `total_seeds` nodes, each node connects to `total_seeds/2` nodes.
+# Similarly, each node receives connections from the other half of the network.
+# That ensures that the connection graph is symmetric and balanced,
+# i.e. each node has `total_seeds/2` outbound and `total_seeds/2` inbound seeds.
+
+total_seeds={{input "clusterSize"}}
+core_prefix=tmapp
+p2p_port=46656
+rpc_port=46657
+
+if [[ ${total_seeds} -le 1 ]]; then
+    echo "not a network, exiting"
+    exit 1
+fi
+
+sleep 5
+echo 'Trying to create p2p meshed connections'
+
+for i in `seq ${total_seeds}`; do
+    seeds=""
+    for j in `seq $(((${total_seeds} - 1) / 2))`; do
+        seeds="${seeds} ${core_prefix}$(((${i} + ${j} - 1) % ${total_seeds} + 1))"
+        [[ ${total_seeds} -eq 2 ]] && break
+    done
+    echo "${core_prefix}${i} connects to ${seeds}"
+    json_msg="seeds=["$(l=$(printf ",\"%s:${p2p_port}\"" ${seeds}); echo ${l:1})"]"
+    url="${core_prefix}${i}:${rpc_port}/dial_seeds"
+    curl --data-urlencode "${json_msg}" "${url}"
+    echo
+    sleep 1
+done
