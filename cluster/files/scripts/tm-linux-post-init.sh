ff9944d3a8857c71a7a480bd46deed1928a04da4
t-bast <bastien@stratumn.com>
2018-01-03 10:48:50 +0100

    Fix file ownership issue

---
+++ cluster/files/scripts/tm-linux-post-init.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+for i in `seq {{input `clusterSize`}}`
+do 
+    docker run -it --rm -v "$(pwd)/tmapp$i/tendermint:/tendermint" -v "$(pwd)/arch/linux/fix-tendermint-files-ownership.sh:/tmp/fix-tendermint-files-ownership.sh" -e "UID=$(id -u)" -e "GID=$(id -g)" --entrypoint=/tmp/fix-tendermint-files-ownership.sh tendermint/tendermint:0.13.0 init
+done
