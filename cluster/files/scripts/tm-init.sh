6caab0bfc47d55abf284b6b760f14fd23b848464
Simon Vadée <simon@stratumn.com>
2018-04-06 10:37:00 +0200

    agent-basic-js, cluster: bump tendermint image to 0.17.1

--- cluster/files/scripts/tm-init.sh
+++ cluster/files/scripts/tm-init.sh
@@ -3,5 +3,5 @@
 # Initialize Tendermint for each node
 for i in `seq {{input `clusterSize`}}`
 do 
-    docker run -it --rm -v "$(pwd)/tmapp$i/tendermint:/tendermint" tendermint/tendermint:0.16.0 init
+    docker run -it --rm -v "$(pwd)/tmapp$i/tendermint:/tendermint" tendermint/tendermint:0.17.1 init
 done
