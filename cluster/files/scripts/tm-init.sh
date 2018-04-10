d3a839687eb1e64b42be45e1f0b3d7c6b9909acf
Alexandre Thibault <pepe@happycoders.org>
2018-04-10 17:49:02 +0200

    *: update to tendermint 0.18.0

--- cluster/files/scripts/tm-init.sh
+++ cluster/files/scripts/tm-init.sh
@@ -1,7 +1,26 @@
 #!/bin/sh
 
+tendermint_version=0.18.0
+
+touch $PWD/dial/keys.sh
+
+p2p_port=46656
+peers=''
+
 # Initialize Tendermint for each node
-for i in `seq {{input `clusterSize`}}`
-do 
-    docker run -it --rm -v "$(pwd)/tmapp$i/tendermint:/tendermint" tendermint/tendermint:0.17.1 init
+for i in `seq {{input `clusterSize`}}`; do 
+    docker run -it --rm -v "$PWD/tmapp$i/tendermint:/tendermint" tendermint/tendermint:${tendermint_version} init
+    # get peer id
+    id=`docker run -it --rm -v "$PWD/tmapp$i/tendermint:/tendermint" tendermint/tendermint:${tendermint_version} show_node_id | dos2unix`
+    # add peers for dial.sh
+    echo "key$i=$id" >> $PWD/dial/keys.sh
+    # add peers as persistent_peers
+    test -n "$peers" && peers="$peers,"
+    peers="$peers$id@tmapp$i:$p2p_port"
+done
+
+# add all peers in each tendermint node configuration
+for f in $PWD/tmapp*/tendermint/config/config.toml; do
+    sed "s/^persistent_peers *= *\".*\"/persistent_peers = \"$peers\"/" $f > $f.tmp
+    mv $f.tmp $f
 done
